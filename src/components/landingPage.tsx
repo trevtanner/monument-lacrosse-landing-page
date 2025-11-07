2a0084fa78ad1361dd43dc0e10ba414e05f53530
Trevor Tanner <trevtanner@gmail.com>
2025-11-07 15:11:29 -0800

    sizing updates

--- src/components/landingPage.tsx
+++ src/components/landingPage.tsx
@@ -7,27 +7,27 @@ export default function LandingPage() {
   return (
     <section
       id="home"
-      className="relative bg-linear-to-b from-primary to-primary text-white py-20 md:py-32 overflow-hidden h-[86vh] w-full"
+      className="relative bg-linear-to-b from-primary to-primary text-white py-12 md:py-24 overflow-hidden min-h-[86vh] w-full"
     >
       <div className="absolute inset-0">
         <Image
-          src="/images/RGHeroImage.png"
+          src="/images/RGHeroImage2.png"
           alt="Custom Apparel Hero"
           className="w-full h-full object-cover opacity-50"
           fill
         />
       </div>
       <div className="container mx-auto relative z-10 text-center px-4 sm:px-6 lg:px-8">
-        <h1 className="text-4xl md:text-6xl font-extrabold leading-tight mb-4 animate-fade-in-up text-white">
+        <h1 className="hero-header-text">
           Monument Lacrosse <br className="hidden sm:inline" />
-          <span className="text-2xl md:text-3xl">
-            Boys and girls youth lacrosse clubs for Northern El Paso County
-          </span>
         </h1>
+        <h2 className="hero-sub-text">
+          Boys and girls youth lacrosse clubs for Northern El Paso County
+        </h2>
 
-        <div className="flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-4 gap-12">
+        <div className="flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-4 gap-x-12 gap-y-8 mt-8">
           <Button
-            className="rounded-full transition duration-300 transform hover:scale-105 bg-black border border-white text-white hover:bg-gray-500 hover:text-white"
+            className="rounded-full transition duration-300 transform hover:scale-110 bg-black border border-white text-white hover:bg-gray-500 hover:text-white w-2/3 sm:w-auto m-auto sm:m-0"
             size="lg"
           >
             <Link
@@ -38,7 +38,7 @@ export default function LandingPage() {
             </Link>
           </Button>
           <Button
-            className="rounded-full transition duration-300 transform hover:scale-105 bg-black border border-white text-white hover:bg-gray-500 hover:text-white"
+            className="rounded-full transition duration-300 transform hover:scale-110 bg-black border border-white text-white hover:bg-gray-500 hover:text-white w-2/3 sm:w-auto m-auto sm:m-0"
             size="lg"
           >
             <Link
